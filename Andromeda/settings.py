407f9464edae6a1d8b98ac0f5bf7c36f87477637
andromedasystem <jmccar16@oldwestbury.edu>
2018-04-29 13:03:42 -0400

    jm2

--- Andromeda/settings.py
+++ Andromeda/settings.py
@@ -103,7 +103,7 @@ WSGI_APPLICATION = 'Andromeda.wsgi.application'
 if 'RDS_DB_NAME' in os.environ:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': os.environ['RDS_DB_NAME'],
             'USER': os.environ['RDS_USERNAME'],
             'PASSWORD': os.environ['RDS_PASSWORD'],
